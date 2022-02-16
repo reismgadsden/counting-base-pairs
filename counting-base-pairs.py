65fc4c2796e1c04e110e1767a3c6bafb6e3fb14e
reismgadsden <rgadsden287@gmail.com>
2022-02-16 14:37:08 -0500

    Used matplotlib to generate a bar plot.

--- counting-base-pairs.py
+++ counting-base-pairs.py
@@ -8,12 +8,14 @@ class: CS-5531-101 @ Appalachian State University
 instructor: Dr. Mohammad Mohebbi
 """
 import os
+import matplotlib.pyplot as plt
 
-sequence_dict = {
+total_sequence_dict = {
     "AT": 0,
     "AU": 0,
     "CG" : 0,
-    "GU" : 0
+    "GU" : 0,
+    "Total": 0
 }
 
 def main() -> None:
@@ -23,43 +25,65 @@ def main() -> None:
     if target_seq.strip().lower() == 'all':
         all_seq = read_all_files()
         for a in all_seq:
-            if not get_indexes(a[0], a[1]):
+            sequence_dict = {
+                "AT": 0,
+                "AU": 0,
+                "CG": 0,
+                "GU": 0,
+                "Total": 0
+            }
+            if not get_indexes(a[0], a[1], sequence_dict):
                 print("Secondary Structure is not Properly Formatted.")
             else:
                 print("Base pair counts for " + a[2])
                 for x in sequence_dict.keys():
                     print("\t- " + x + ": " + str(sequence_dict[x]))
+        print("\nTotal pair counts")
+        for x in total_sequence_dict.keys():
+            if x != "Total":
+                print("\t- " + x + ": " + str(total_sequence_dict[x]) + "\n\t\t% of Total: "
+                      + str((total_sequence_dict[x]/ total_sequence_dict["Total"]) * 100) + "%")
+            else:
+                print("\t- " + x + ": " + str(total_sequence_dict[x]))
+        plot_dict(total_sequence_dict)
     else:
         sequence, secondary_struct = read_data(target_seq)
         if not get_indexes(sequence, secondary_struct):
             print("Secondary Structure is not Properly Formatted.")
         else:
             print("Base pair counts for " + target_seq)
-            for x in sequence_dict.keys():
-                print("\t- " + x + ": " +str(sequence_dict[x]))
+            for x in total_sequence_dict.keys():
+                print("\t- " + x + ": " +str(total_sequence_dict[x]))
 
 
-def get_indexes(sequence, second_struct) -> bool:
+def get_indexes(sequence, second_struct, single_dict=None) -> bool:
     stack = []
     i = 0
     for x in second_struct:
         if x == "(":
             stack.append(i)
         elif x == ")":
-            count_sequences(stack.pop(), i, sequence)
+            if single_dict is None:
+                count_sequences(stack.pop(), i, sequence)
+            else:
+                count_sequences(stack.pop(), i, sequence, single_dict)
         i += 1
     return len(stack) == 0
 
 
 
-def count_sequences(open_index, close_index, sequence) -> None:
+def count_sequences(open_index, close_index, sequence, single_dict=None) -> None:
     open_seq_char = sequence[open_index].upper()
     close_seq_char = sequence[close_index].upper()
     if ord(open_seq_char) > ord(close_seq_char):
         temp = close_seq_char
         close_seq_char = open_seq_char
         open_seq_char = temp
-    sequence_dict[open_seq_char + close_seq_char] += 1
+    total_sequence_dict[open_seq_char + close_seq_char] += 1
+    total_sequence_dict["Total"] += 1
+    if single_dict is not None:
+        single_dict[open_seq_char + close_seq_char] += 1
+        single_dict["Total"] += 1
 
 
 def read_data(filename) -> tuple:
@@ -73,11 +97,12 @@ def read_data(filename) -> tuple:
         if try_struct_2 not in dir_content:
             print("Invalid file name, exiting program.")
             exit(0)
-
     file_path = dir_path + "\\" + valid_name
     f = open(file_path, 'r')
     output = f.readlines()
     f.close()
+
+    return output[1], output[2]
 
 
 def read_all_files() -> list:
@@ -92,6 +117,23 @@ def read_all_files() -> list:
         f.close()
 
     return container
+
+
+def plot_dict(dict) -> None:
+    new_dict = dict
+
+    for x in new_dict:
+        if x != "Total":
+            new_dict[x] = (new_dict[x]/new_dict["Total"]) * 100
+    del dict["Total"]
+
+    pairs = list(new_dict.keys())
+    counts = list(new_dict.values())
+
+    plt.bar(range(len(new_dict)), counts, tick_label=pairs)
+    plt.ylim([0, 100])
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
